478a0205668e0e31faa7b269f1d7bade2462e8d9
nerdiejack <nerdiejack@gmail.com>
2015-03-13 15:23:53 +0100

    Add cron pull/apply

---
+++ modules/puppet/files/pull-updates.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /root/puppet
+git pull && /usr/local/bin/papply
--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -1,6 +1,26 @@
 class puppet {
+	
 	file { '/usr/local/bin/papply':
 		source	=> 'puppet:///modules/puppet/papply.sh',
 		mode	=> '0755',
+	
 	}
+	
+	file { '/usr/local/bin/pull-updates':
+		source	=> 'puppet:///modules/puppet/pull-updates.sh',
+		mode	=> '0755',
+	}
+
+	file { '/root/.ssh/id_rsa':
+		source  => 'puppet:///modules/puppet/debian.priv',
+		owner	=> '0600',
+	}
+
+	cron { 'run-puppet':
+		ensure	=> 'present',
+		user	=> 'root',
+		command	=> '/usr/local/bin/pull-updates',
+		minute	=> '*/10',
+		hour	=> '*',
+	}	
 }
