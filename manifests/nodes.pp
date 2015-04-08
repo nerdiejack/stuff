8b5f2e58417cec1fd22881651e010195ecc044f0
nerdiejack <nerdiejack@gmail.com>
2015-04-08 13:55:12 +0200

    Add OS check

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,3 +1,11 @@
+if $::operatingsystem in [ 'Ubuntu', 'Debian' ] {
+  notify {  'Debian-type operating system detected': }
+} elsif $::operatingssytem in [ 'Redbat', 'Fedora', 'SuSE', 'CentOS' ] {
+  notify { 'RedHat-type operating system detected': }
+} else {
+  notify { 'Some other operating system detcted': }
+}
+
 node 'cookbook2', 'cookbook3' { 
 	include puppet
 }
